5a38d155e2993400bb2f36a720a911e3fda71fe1
arisa-e <emmanuelarisa@outlook.com>
2022-11-02 23:22:47 +0300

    update logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import { Stack } from '@mui/material'
 import React from 'react'
 import { Link } from 'react-router-dom'
+import logo from "../assets/Logo.png"
 
 const Navbar = () => {
   return (
@@ -9,7 +10,7 @@ const Navbar = () => {
       justifyContent='space-around' 
       sx={{gap:{sm:'122px',xs:'40px'}, mt:{sm: '32px', xs:'20px'}, justifyContent: 'none'}}>
       <Link to ='/'>
-        <img src="" alt="" style={{width: '48px',height: '48px', margin:'0 20px'}}/>
+        <img src={logo} alt="" style={{width: '48px',height: '48px', margin:'0 20px'}}/>
       </Link>
       <Stack direction='row' gap='40px' fontSize='24px' alignItems='flex-end'>
         <Link to ="/" style={{textDecoration:'none', color:'#3A1212', borderBottom: '3px solid #FF2625'}}>Home</Link>
