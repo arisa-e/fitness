3da3de0934903112a6faad074d7c37a0ff9bd0ff
arisa-e <emmanuelarisa@outlook.com>
2022-11-15 23:28:37 +0300

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,5 @@ export {default as Footer} from "./Footer"
 export {default as HeroBanner} from "./HeroBanner"
 export {default as SearchExercise} from "./SearchExercises"
 export {default as Exercise} from './Exercise'
+export {default as HorizontalScrollbar} from './HorizontalScrollbar'
+export {default as BodyPart} from "./BodyPart"
